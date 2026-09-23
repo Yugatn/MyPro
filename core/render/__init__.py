dff2280e71fbfa28d6499b19bae7787cd7f02cb0
Yugatn <yug.atn@gmail.com>
2026-09-23 13:23:51 +0400

    feat: add FFmpeg render backend foundation

---
+++ core/render/__init__.py
@@ -0,0 +1,6 @@
+"""Render specifications and backend contracts."""
+from .specification import RenderSpecification
+from .result import RenderResult
+from .backend import RenderBackend
+
+__all__ = ["RenderSpecification", "RenderResult", "RenderBackend"]
